b89e7a16b0feea4c2726214c335b7082c66334d3
hutteg <hutteg@d5079a12-bd52-11dd-b9c8-e7e19bf6fede>
2008-12-29 17:37:49 +0000

    Zutaten werden nun mit voller unterstuetzung des HSE Frameworks bearbeitet.

---
+++  fhj-swd05-semsd-hse-rezeptdatenbank/hse-minimal/src/fhj/swd05/hutteg/rezeptdb/zutat/ZutatEditRequestor.java
@@ -0,0 +1,137 @@
+package fhj.swd05.hutteg.rezeptdb.zutat;
+
+
+import nextapp.echo2.app.Column;
+import nextapp.echo2.app.ContentPane;
+import nextapp.echo2.app.Extent;
+import nextapp.echo2.app.Grid;
+import nextapp.echo2.app.Insets;
+import nextapp.echo2.app.Label;
+import nextapp.echo2.app.TextField;
+import nextapp.echo2.app.event.ActionEvent;
+
+import org.stenerud.hse.base.ui.echo2.component.StaticTable;
+import org.stenerud.hse.base.ui.echo2.component.TrueFalseRequestor;
+import org.stenerud.hse.base.ui.echo2.tool.TwoColumnDataHelper;
+import org.stenerud.hse.base.ui.echo2.validation.ValidationRuleMaker;
+import org.stenerud.hse.base.ui.validation.ValidationException;
+import org.stenerud.hse.base.ui.validation.Validator;
+
+import echopointng.TabbedPane;
+import echopointng.tabbedpane.DefaultTabModel;
+
+public class ZutatEditRequestor extends TrueFalseRequestor{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// Private Stuff
+	private TextField einheitTextField = null;
+	private TextField energieMengeTextField = null; // Textfield, damit man die Formularvalidierung testen kann
+	private Validator validator = new Validator();
+
+	private Zutat activeZutat;
+	// ctor
+	public ZutatEditRequestor() {
+	}
+
+	// muss ueberschrieben werden um Inhalte hinzuzufuegen
+	protected void initContents(Column contents)
+	{
+		
+		setTrueString("Speichern");
+		setFalseString("Abbrechen");
+		setResizable(true);
+
+		ValidationRuleMaker rules = new ValidationRuleMaker(validator);
+
+		StaticTable table = new StaticTable(2);
+		contents.add(table);
+		table.setStyleName("TwoColumn.Table");
+
+		TwoColumnDataHelper dataHelper = new TwoColumnDataHelper(messages, "TwoColumn.FieldName", "TwoColumn.FieldValue");
+
+		einheitTextField = dataHelper.addTextField(table, "zutat.einheit", 15, -1);
+		einheitTextField.setWidth(new Extent(200, Extent.PX));
+		rules.addNotEmptyRule("zutat.einheit", einheitTextField);
+
+		energieMengeTextField = dataHelper.addTextField(table, "zutat.energiemenge", 15, -1);
+		energieMengeTextField.setWidth(new Extent(200, Extent.PX));
+		rules.addIntegerRule("zutat.energiemenge", energieMengeTextField);
+		
+	}
+	protected void resetComponents()
+	{
+		// Nothing to do
+	}
+	
+	// Kann ueberschrieben werden
+	protected boolean handleAction(ActionEvent ev)
+	{
+		if ( !ev.getActionCommand().equals(COMMAND_FALSE) )
+		{
+			try
+			{
+				validator.validate(messages);
+				
+				// Wenn wir hier ankommen, hat alles gepasst und wir koennen die Eigenschaften am
+				// Businessobjekt setzen
+				activeZutat.setEinheit(getEinheit());
+				activeZutat.setEnergiemenge(Integer.parseInt(getEnergiemenge()));
+			}
+			catch ( ValidationException ex )
+			{
+				handleValidationException(ex);
+				return false;
+			}
+			catch ( RuntimeException ex )
+			{
+				if ( !handleException(ex) )
+				{
+					throw ex;
+				}
+				return false;
+			}
+		}
+		return true;
+
+	}
+	
+	// == Getters and Setters ==
+	public void setEinheit(String newValue)
+	{
+		einheitTextField.setText(newValue);
+	}
+	
+	public String getEinheit()
+	{
+		return einheitTextField.getText();
+	}
+	
+	public void setEnergiemenge(String newValue)
+	{
+		energieMengeTextField.setText(newValue);
+	}
+	
+	public String getEnergiemenge()
+	{
+		return energieMengeTextField.getText();
+	}
+
+	/**
+	 * Wird vom Aufrufenden Objekt gesetzt. Dient als Referenz auf die zu aendernde Zutat
+	 * @param zutat
+	 */
+	public void setActiveZutat(Zutat zutat) {
+		this.activeZutat = zutat;
+		setEinheit(zutat.getEinheit());
+		setEnergiemenge(String.valueOf(zutat.getEnergiemenge()));
+		setTitle("Zutat : " + zutat.getName());
+	}
+
+	public Zutat getActiveZutat() {
+		return activeZutat;
+	}
+}
---  fhj-swd05-semsd-hse-rezeptdatenbank/hse-minimal/src/fhj/swd05/hutteg/rezeptdb/zutat/ZutatScreen.java
+++  fhj-swd05-semsd-hse-rezeptdatenbank/hse-minimal/src/fhj/swd05/hutteg/rezeptdb/zutat/ZutatScreen.java
@@ -1,6 +1,20 @@
 package fhj.swd05.hutteg.rezeptdb.zutat;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import nextapp.echo2.app.Button;
+import nextapp.echo2.app.Column;
+import nextapp.echo2.app.Component;
+import nextapp.echo2.app.Row;
+import nextapp.echo2.app.event.ActionEvent;
+import nextapp.echo2.app.event.ActionListener;
+
+import org.stenerud.hse.base.ui.echo2.BaseApplicationHelper;
+import org.stenerud.hse.base.ui.echo2.screen.PaneScreen;
+import org.stenerud.hse.base.ui.echo2.screen.ScreenManager;
 
 import echopointng.TabbedPane;
 import echopointng.tabbedpane.DefaultTabModel;
@@ -12,73 +26,134 @@ import echopointng.tabbedpane.DefaultTabModel;
  * 
  * @author You
  */
-public class ZutatScreen extends org.stenerud.hse.base.ui.echo2.screen.PaneScreen
+public class ZutatScreen extends PaneScreen implements ActionListener
 {
-	public ZutatDAO getZutatDAO() {
-		return zutatDAO;
-	}
-
-	public void setZutatDAO(ZutatDAO zutatDAO) {
-		this.zutatDAO = zutatDAO;
-	}
 
 	private static final long serialVersionUID = 1L;
+
+	// == INJECTED MEMBERS ==
 	private ZutatDAO zutatDAO = null;
-	DefaultTabModel defaultTabModel = new DefaultTabModel();
-	private TabbedPane zutatpane = new TabbedPane();
+	private ZutatEditRequestor zutatEditor = null;
+	
+	//== PRIVATE MEMBERS ==
 
-	public ZutatDAO getZutatDao() {
-		return zutatDAO;
+
+	@SuppressWarnings("unchecked")
+	protected void initComponents()
+	{
+		List<Zutat> zutaten = zutatDAO.getZutaten();
+		TabbedPane zutatpane = new TabbedPane();
+		DefaultTabModel dtm = new DefaultTabModel();
+		
+		dtm.addTab("A-E", createColumnWithZutaten(zutaten, "[A-E].*"));
+		dtm.addTab("F-N", createColumnWithZutaten(zutaten, "[F-N].*"));
+		dtm.addTab("O-T", createColumnWithZutaten(zutaten, "[O-T].*"));
+		dtm.addTab("U-Z", createColumnWithZutaten(zutaten, "[U-Z].*"));
+		
+		zutatpane.setModel(dtm);
+		zutatpane.setSelectedIndex(0);
+		add(zutatpane);
+		
+		// ActionListener fuer Zutateneditor hinyufuegen
+		zutatEditor.addActionListener(this);
+		
+	}
+	/*
+	 * Eryeugt eine Column mit den Zutatenbuttons
+	 * @pattern eine Regular Expression
+	 */
+	private Component createColumnWithZutaten(List<Zutat> zutaten, String pattern) {
+		Column col = new Column();
+		Pattern p = Pattern.compile(pattern,Pattern.CASE_INSENSITIVE);
+
+		for(Zutat z : zutaten)
+		{
+			// Wenn das Pattern passt wird was zurueckgegeben
+			if(p.matcher(z.getName()).matches())
+			{
+				Row r = createZutatRowForZutatObject(z);
+				col.add(r);
+			}
+		}
+		return col;
+	}
+	/*
+	 * Erzeugt eine Zeile fuer ein Zutatenobjekt 
+	 */
+	private Row createZutatRowForZutatObject(final Zutat zutat)
+	{
+		Row r = new Row();
+		Button b = new Button(zutat.getName());
+		b.addActionListener(new ActionListener()
+		{
+			private static final long serialVersionUID = 1L;
+			public void actionPerformed(ActionEvent e)
+			{
+				processZutatenEdit(zutat);
+			}
+		});
+		r.add(b);
+		return r;
 	}
 
-	public void setZutatDao(ZutatDAO zutatDao) {
-		this.zutatDAO = zutatDao;
+	/*
+	 * Wird von den 
+	 */
+	private void processZutatenEdit(Zutat zutat) {
+		System.out.println("Die Zutat " + zutat.getName() + " wird editiert");
+		
+		BaseApplicationHelper.displayRequestor(zutatEditor);
+		zutatEditor.setActiveZutat(zutat);
+		System.out.println("Die Zutat " + zutat.getName() + " wurde editiert");		
+		
 	}
+	protected void resetComponents()
+	{
+		// Nothing to do.
+	}
+	
 
 	public String getTitle()
 	{
 		// Remember to set screen.widgets in Messages.properties!
 		return messages.get("screen.zutat");
 	}
-
-	@SuppressWarnings("unchecked")
-	protected void initComponents()
-	{
-
-		List<Zutat> zutaten = zutatDAO.getZutaten();
-		
-		String firstLetter = "::::::"; // Ein Rezept das es bestimmt nicht gibt
-		ZutatChooserGUI rgui = null;
-		
-		for(Zutat z : zutaten)
-		{
-			if(z.getName().startsWith(firstLetter))
-			{
-				// es sollte schon ein Tab geben, das rezept gehoert dahin
-				rgui.addZutat(z);
-			}
-			else
-			{
-				// scheinbar gibt es noch kein Tab, also machen wir ein neues
-				firstLetter = String.valueOf(z.getName().charAt(0));
-				rgui = new ZutatChooserGUI(this.zutatDAO);
-				rgui.addZutat(z);
-				defaultTabModel.addTab(firstLetter, rgui);
-			}
-		}
-		this.zutatpane.setModel(defaultTabModel);
-	    
-		// Wenn mindestens ein Tab hinzugefuegt wurde wird das erste Tab angezeigt
-		if(rgui != null)
-		{
-			this.zutatpane.setSelectedIndex(0);
-		}
-		add(this.zutatpane);
+	
+	// == GETTERS AND SETTERS ==
+	
+	public ZutatEditRequestor getZutatEditor() {
+		return zutatEditor;
 	}
-
-	protected void resetComponents()
-	{
-		// Nothing to do.
+	
+	public void setZutatEditor(ZutatEditRequestor zutatEditor) {
+		this.zutatEditor = zutatEditor;
 	}
-
+	
+	public ZutatDAO getZutatDAO() {
+		return zutatDAO;
+	}
+	
+	public void setZutatDAO(ZutatDAO zutatDAO) {
+		this.zutatDAO = zutatDAO;
+	}
+	/** 
+	 * Wird vom Zutateneditor aufgerufen
+	 */
+	@Override
+	public void actionPerformed(ActionEvent actionevent) {
+		// TODO Auto-generated method stub
+		
+		// Nur JA Events interessieren uns hier
+		if ( !actionevent.getActionCommand().equals(ZutatEditRequestor.COMMAND_FALSE) )
+		{
+			zutatDAO.update(zutatEditor.getActiveZutat());
+		}
+		else
+		{
+			// Sonst werden die alten Werte wieder aus der DB geladen
+			zutatDAO.refresh(zutatEditor.getActiveZutat());
+		}
+		// Wie auch immer der Benutzer gewaehlt hat, der Editor wird geschlossen
+		zutatEditor.dispose();
+	}
 }
