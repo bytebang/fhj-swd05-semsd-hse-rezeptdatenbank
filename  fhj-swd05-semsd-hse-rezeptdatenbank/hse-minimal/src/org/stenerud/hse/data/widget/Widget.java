b00465a317393eb59d2bbcc08435410c66fa5fe5
hutteg <hutteg@d5079a12-bd52-11dd-b9c8-e7e19bf6fede>
2009-01-31 15:15:03 +0000

    finale Version

---  fhj-swd05-semsd-hse-rezeptdatenbank/hse-minimal/src/org/stenerud/hse/data/widget/Widget.java
+++  fhj-swd05-semsd-hse-rezeptdatenbank/hse-minimal/src/org/stenerud/hse/data/widget/Widget.java
@@ -1,6 +1,15 @@
 package org.stenerud.hse.data.widget;
 
+/**
+ *  Hier werden businesspezifische Methoden implementiert
+ *  sofern dies notwendig ist.
+ *  
+ *  In diesem Beispiel wird hier nur eine ID und ein NAME Feld angelegt.
+ *  Damit kann gezeigt werden wie Hibernate, Spring, und Echo2 ... also HSE
+ *  konfiguriert werden müssen um korrekt zu funktionieren.
+ */
 public class Widget {
+
 	private int id;
 	private String name;
 	public String getName() {
